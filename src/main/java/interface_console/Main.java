898366e87b526637a22a12f36ce8050b3f178f98
William Buron <buronwilliam22@gmail.com>
2019-04-16 15:37:02 +0200

    interface un peu mieux

--- src/main/java/interface_console/Main.java
+++ src/main/java/interface_console/Main.java
@@ -6,29 +6,18 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Bonjour ! Que souhaitez-vous faire ?");
-		System.out.println("Au menu :");
-		System.out.println("1- Ajouter/modifier/rechercher un étudiant");
-		System.out.println("2- Ajouter/modifier/rechercher un établissement");
-		System.out.println("3- Ajouter/modifier/rechercher une formation");
-		System.out.println("4- Lister l'ensemble des étudiants");
-		System.out.println("5- Lister l'ensemble des formations");
-		System.out.println("6- Lister l'ensemble des universités");
-		System.out.println("7- Extraire data sous format XML");
-		System.out.println("8- Importation data depuis XML file");
-		System.out.println("9- Lister les cours d'un étudiant ... (a voir)");
-		System.out.println("10- Quitter");
-
+		System.out.println("Bonjour !");
+		
 		Scanner clavier = new Scanner(System.in);
-
-		System.out.println("-----------------------------------------------");
+		
 		int choix = 0;
 		while (choix != 10) {
+			printChoix();
 			choix = clavier.nextInt();
 			int secondChoix = 0;
 			switch (choix) {
 			case 1:
-				System.out.println("Aller ! On recommence : ");
+				System.out.println("Et maintenant ?");
 				System.out.println("1- Ajouter un étudiant");
 				System.out.println("2- Modifier un étudiant");
 				System.out.println("3- Rechercher un étudiant");
@@ -45,6 +34,8 @@ public class Main {
 						break;
 					case 3:
 						// TODO : rechercher un étudiant
+						break;
+					case 4:
 						break;
 					default:
 						System.out.println("Tu sais pas lire ?");
@@ -71,14 +62,17 @@ public class Main {
 					case 3:
 						// TODO : rechercher un établissement
 						break;
+					case 4:
+						break;
 					default:
 						System.out.println("Tu sais pas lire ?");
+						System.out.println("-----------------------------------------------");
 					}
 				}
 				break;
 			case 3:
 				while (secondChoix != 4) {
-					System.out.println("Aller ! On recommence : ");
+					System.out.println("Et maintenant ?");
 					System.out.println("1- Ajouter un formation");
 					System.out.println("2- Modifier un formation");
 					System.out.println("3- Rechercher un formation");
@@ -95,8 +89,11 @@ public class Main {
 					case 3:
 						// TODO : rechercher un formation
 						break;
+					case 4:
+						break;
 					default:
 						System.out.println("Tu sais pas lire ?");
+						System.out.println("-----------------------------------------------");
 					}
 				}
 				break;
@@ -118,6 +115,8 @@ public class Main {
 			case 9:
 				// TODO or not TODO : les cours d'un étudiant
 				break;
+			case 10:
+				break;
 			default:
 				System.out.println("Tu sais pas lire ?");
 			}
@@ -126,6 +125,21 @@ public class Main {
 		System.out.println("DATA SAVE !");
 		System.out.println("BYE !");
 
+	}
+	
+	public static void printChoix(){
+		System.out.println("Que souhaitez-vous faire ? Au menu :");
+		System.out.println("1- Ajouter/modifier/rechercher un étudiant");
+		System.out.println("2- Ajouter/modifier/rechercher un établissement");
+		System.out.println("3- Ajouter/modifier/rechercher une formation");
+		System.out.println("4- Lister l'ensemble des étudiants");
+		System.out.println("5- Lister l'ensemble des formations");
+		System.out.println("6- Lister l'ensemble des universités");
+		System.out.println("7- Extraire data sous format XML");
+		System.out.println("8- Importation data depuis XML file");
+		System.out.println("9- Lister les cours d'un étudiant ... (a voir)");
+		System.out.println("10- Quitter");
+		System.out.println("-----------------------------------------------");
 	}
 
 }
